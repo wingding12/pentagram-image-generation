aea2ad3f49c9ce7e4807b98b37e675e5ae2b22f7
wingding12 <144286538+wingding12@users.noreply.github.com>
2024-12-21 18:16:06 -0500

    Project updated for Vervel Live App

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import ImageGenerator from "./components/imageGenerator";
-import { generateImage } from "./actions/generateImage";
-
-export default async function Home() {
-  return <ImageGenerator generateImage={generateImage} />;
-}
